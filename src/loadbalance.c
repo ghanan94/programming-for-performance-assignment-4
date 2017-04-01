993e5e485f2e862b379a0893f76483a376228c2d
Ghanan Gowripalan <ghanan94@gmail.com>
2017-04-01 19:54:53 -0400

    add to queue using a tail pointer

--- src/loadbalance.c
+++ src/loadbalance.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 500 
+#define _XOPEN_SOURCE 500
 
 #include <stdlib.h>
 #include <pthread.h>
@@ -32,6 +32,7 @@ typedef struct job_t {
 
 typedef struct {
     job_t* head;
+    job_t* tail;
     pthread_mutex_t* mutex;
 } queue_t;
 
@@ -59,6 +60,17 @@ void enqueue( job_t* toEnqueue, unsigned int* generator_seed );
 
 queue_t * queues;
 pthread_t * threads;
+
+void add_to_queue(queue_t* queue, job_t* job) {
+    pthread_mutex_lock(queue->mutex);
+    if (queue->head == NULL) {
+        queue->head = job;
+    } else {
+        queue->tail->next = job;
+    }
+    queue->tail = job;
+    pthread_mutex_unlock(queue->mutex);
+}
 
 /* error handling macro from Patrick Lam */
 void abort_(const char * s, ...) {
@@ -162,7 +174,7 @@ int main(int argc, char **argv) {
     }
   }
 
-    printf("Starting up with %d queues, assignment policy %d, %d jobs, lambda %d, max rounds %d, and load balancing %d.\n", 
+    printf("Starting up with %d queues, assignment policy %d, %d jobs, lambda %d, max rounds %d, and load balancing %d.\n",
         num_queues, policy, num_jobs, lambda, max_rounds, balance_load);
 
     csv = fopen(OUTPUT_FILE_NAME, "w+");
@@ -172,7 +184,7 @@ int main(int argc, char **argv) {
 
     queues = malloc( num_queues * sizeof( queue_t ) );
     threads = malloc( num_queues * sizeof( pthread_t ) );
-   
+
     for ( int i = 0; i < num_queues; ++i ) {
         queues[i].head = NULL;
         queues[i].mutex = malloc( sizeof( pthread_mutex_t ) );
@@ -182,7 +194,7 @@ int main(int argc, char **argv) {
     for ( int j = 0; j < num_queues; ++j ) {
         pthread_create( &threads[j], NULL, fetch_and_execute, &queues[j]);
     }
-    
+
     pthread_create( &generator, NULL, generate, NULL);
 
     if ( 1 == balance_load ) {
@@ -199,7 +211,7 @@ int main(int argc, char **argv) {
         pthread_mutex_destroy(queues[l].mutex);
         free(queues[l].mutex);
     }
-    
+
     free(queues);
 
     if ( 1 == balance_load ) {
@@ -230,25 +242,25 @@ void *fetch_and_execute( void* arg ) {
             execute( job );
             write_to_file( job );
         }
-    
+
     }
     pthread_exit( NULL );
 }
 
 void execute( job_t* job ) {
- 
+
     struct timeval begin_execution;
-    gettimeofday( &begin_execution, NULL ); 
+    gettimeofday( &begin_execution, NULL );
     unsigned char* output_buffer = calloc( HASH_BUFFER_LENGTH , sizeof ( unsigned char ) );
 
     for( int i = 0; i < job->rounds; ++i ) {
         SHA256( job->data, HASH_BUFFER_LENGTH, output_buffer );
         memcpy( job->data, output_buffer, HASH_BUFFER_LENGTH );
-	}
+    }
     job->output = output_buffer;
 
     gettimeofday( &job->departure_time, NULL );
-    timeval_subtract (&job->execution_time, &(job->departure_time), &(begin_execution)); 
+    timeval_subtract (&job->execution_time, &(job->departure_time), &(begin_execution));
 }
 
 void *generate( void* arg ) {
@@ -261,7 +273,7 @@ void *generate( void* arg ) {
         ++id;
 
         new_job->rounds = ceil( (double)rand_r(&generator_seed)/(double)RAND_MAX * max_rounds );
-        new_job->data = random_string( HASH_BUFFER_LENGTH, &generator_seed ); 
+        new_job->data = random_string( HASH_BUFFER_LENGTH, &generator_seed );
 
         enqueue( new_job, &generator_seed );
 
@@ -276,12 +288,12 @@ void *generate( void* arg ) {
 }
 
 void write_to_file( job_t* job ) {
-    
+
     struct timeval response_time;
-    timeval_subtract (&response_time, &(job->departure_time), &(job->arrival_time)); 
-    
+    timeval_subtract (&response_time, &(job->departure_time), &(job->arrival_time));
+
     pthread_mutex_lock( &completed_mutex );
-    
+
     /* Write to file should probably be serialized because jumbled output is bad.*/
     fprintf( csv, "%d,", job->id );
     fprintf( csv, "%ld.%06ld", job->arrival_time.tv_sec, job->arrival_time.tv_usec );
@@ -309,26 +321,16 @@ void write_to_file( job_t* job ) {
 void enqueue( job_t* job, unsigned int * generator_seed ) {
 
     queue_t* selected;
-    
+
     if (RANDOM_ASSIGNMENT == policy ) {
-        selected = &queues[ rand_r(generator_seed) % num_queues ]; 
+        selected = &queues[ rand_r(generator_seed) % num_queues ];
     } else if ( ROUND_ROBIN_ASSIGNMENT == policy ) {
         selected = &queues[ job->id % num_queues ];
     } else {
        abort_("[enqueue] Invalid assignment policy selected: %d\n", policy);
     }
 
-    pthread_mutex_lock(selected->mutex);
-    if (selected->head == NULL) {
-        selected->head = job;
-    } else {
-        job_t* j = selected->head;
-        while ( j->next != NULL ) {
-            j = j->next;
-        }
-        j->next = job;
-    }
-    pthread_mutex_unlock(selected->mutex);
+    add_to_queue(selected, job);
 }
 
 void *load_balance( void* args ) {
@@ -336,5 +338,3 @@ void *load_balance( void* args ) {
 
     pthread_exit( NULL );
 }
-
-
